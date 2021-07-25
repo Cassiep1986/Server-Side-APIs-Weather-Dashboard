fa07edf54f6d9af98021f45579ddda1d720d9604
seacrest <cassiep1986@gmail.com>
2021-07-25 16:18:26 -0700

    targeted form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,41 @@
-var repoList = document.querySelector('ul');
-var fetchButton = document.getElementById('fetch-button');
+var forecast = document.querySelector('.forecast');
+var searchButton = document.getElementById('searchbtn');
+var input = document.querySelector("#searchbox")
+
 
 function getApi() {
   // replace `octocat` with anyone else's GitHub username
-  var requestUrl = 'https://api.github.com/users/octocat/repos';
+  console.log("button pressed");
+  var requestUrl = `api.openweathermap.org/data/2.5/forecast?q=${data}&appid={3301ee6fc116327db709dae75404298b}`;
 
-  fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      for (var i = 0; i < data.length; i++) {
-        var listItem = document.createElement('li');
-        listItem.textContent = data[i].html_url;
-        repoList.appendChild(listItem);
-      }
-    });
-}
+searchButton.addEventListener('click', function () {
+  var data = input.value;
+  console.log("button pressed");
+  console.log(data);
+  // var requestUrl = `api.openweathermap.org/data/2.5/forecast?q=${data}&appid={3301ee6fc116327db709dae75404298b}`;
 
-fetchButton.addEventListener('click', getApi);
+  // fetch(requestUrl)
+  //   .then(function (response) {
+  //     return response.json();
+  //   })
+  //   .then(function (data) {
+  //     for (var i = 0; i < data.length; i++) {
+  //       var listItem = document.createElement('li');
+  //       listItem.textContent = data[i].html_url;
+  //       repoList.appendChild(listItem);
+  //     }
+  //   });
+});
+//**other option below */
+//   fetch(requestUrl)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       for (var i = 0; i < data.length; i++) {
+//         var listItem = document.createElement('li');
+//         listItem.textContent = data[i].html_url;
+//         repoList.appendChild(listItem);
+//       }
+//     });
+// }
