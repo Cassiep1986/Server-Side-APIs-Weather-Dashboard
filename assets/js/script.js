ce3f313cd4fd4a1829997cec5aae309b2262fb8e
seacrest <cassiep1986@gmail.com>
2021-07-28 00:25:05 -0700

    updated data fields, implemented momentjs

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,68 +6,93 @@ var Temp = document.querySelector("#temp");
 var Wind = document.querySelector("#wind");
 var Humidity = document.querySelector("#humidity");
 var Uvindex = document.querySelector("#uv_index");
+var test = document.querySelector("#test");
+var savedCities = JSON.parse(localStoreage.getItem())
 
-searchButton.addEventListener("click", function () {
-  var city = input.value;
-
-  var weatherurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3301ee6fc116327db709dae75404298b&units=imperial`;
-
-  fetch(weatherurl)
-    .then(function (response) {
-      // console.log(response);
-      return response.json();
-    })
-    .then(function (data) {
-      // console.log(data);
-      Cityname.append(data.name);
-      Temp.append(Math.round(data.main.temp));
-      Wind.append(data.wind.speed);
-      Humidity.append(data.main.humidity);
-
-      let lat = data.coord.lat;
-      let lon = data.coord.lon;
-
-      var onecallurl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=3301ee6fc116327db709dae75404298b&units=imperial`
-
-      fetch(onecallurl)
-      .then(function (response){
+  function getWeather() {
+    var city = input.value;
+  
+    var weatherurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3301ee6fc116327db709dae75404298b&units=imperial`;
+  
+    fetch(weatherurl)
+      .then(function (response) {
+        // console.log(response);
         return response.json();
       })
-      .then(function (data){
+      .then(function (data) {
+        // console.log(data);
+        Cityname.append(data.name);
+        Temp.append("   " + (Math.round(data.main.temp)) + " \xB0 F") ;
+        Wind.append("   " + (data.wind.speed) + " mph");
+        Humidity.append("   " + (data.main.humidity) + " %") ;
+  
+        let lat = data.coord.lat;
+        let lon = data.coord.lon;
+  
+        var onecallurl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=3301ee6fc116327db709dae75404298b&units=imperial`
+  
+        fetch(onecallurl)
+        .then(function (response){
+          return response.json();
+        })
+        .then(function (data){
+          console.log(data);
+          Uvindex.append("   " + (data.current.uvi));
+          forecastFive(data);
+          getdates();
+        })
+      });
+  };
+
+    function forecastFive(data) {
+      for (let i = 0; i < 5; i++) {
+        var day = "#day" + [i]; 
+        var dailyfive = document.querySelector(day);
         
-        console.log(data);
-        Uvindex.append(data.current.uvi);
-      })
-    });
-  });
+        var hightemp = document.createElement("li"); 
+        hightemp.textContent= "High: " + data.daily[i].temp.max + "	\xB0 F";
 
-//** Other fetch syntax */
-// function getApi() {
+        var lowtemp = document.createElement("li"); 
+        lowtemp.textContent= "Low: " + data.daily[i].temp.min + " \xB0 F"; 
 
-//   console.log("button pressed");
-//   var requestUrl = `api.openweathermap.org/data/2.5/forecast?q=${data}&appid={3301ee6fc116327db709dae75404298b}`;
+        dailyfive.append(hightemp);
+        dailyfive.append(lowtemp);
+    };
 
-//   fetch(requestUrl)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       for (var i = 0; i < data.length; i++) {
-//         var listItem = document.createElement('li');
-//         listItem.textContent = data[i].html_url;
-//         repoList.appendChild(listItem);
-//       }
-//     });
-// }
+    for (let i = 0; i < 5; i++) {
+      var day = "#day" + [i]; 
+      var dailyfive = document.querySelector(day);
+      
+      var dailywind = document.createElement("li"); 
+      dailywind.textContent= "Wind:   " + data.daily[i].wind_speed + " mph";
 
-//**** */
-// .then(function (data) {
-//   console.log(data);
-//   Cityname.append(data.name);
-//   Temp.append(Math.round(data.main.temp));
-//   Wind.append(data.wind.speed);
-//   Humidity.append(data.main.humidity);
+      dailyfive.append(dailywind);
+  };
 
-//   }
-// );
-// });
+  for (let i = 0; i < 5; i++) {
+    var day = "#day" + [i]; 
+    var dailyfive = document.querySelector(day);
+    
+    var dailyhumidity = document.createElement("li"); 
+    dailyhumidity.textContent= "Humidity:   " + data.daily[i].humidity + " %";
+
+    dailyfive.append(dailyhumidity);
+};
+    };
+  
+    function getdates() {
+      for (let i = 0; i < 5; i++) {
+        var dayofweek = "#dayofweek" + [i]; 
+        var smtwtfs = document.querySelector(dayofweek);
+        var newday = (moment().add([i]).format("dddd"));
+        // var newday = ("moment()" + ".add" + ([i]))
+        var SMTWTFS = document.createElement("p"); 
+        SMTWTFS.textContent= newday;
+    
+        smtwtfs.append(SMTWTFS);
+    }; 
+    }
+
+
+  searchButton.addEventListener("click", getWeather);
+
