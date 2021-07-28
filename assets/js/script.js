c8dd8354fe7d7052fef579d0ee77d44e7cec8423
seacrest <cassiep1986@gmail.com>
2021-07-27 18:03:30 -0700

    added api info, added cards for 5 day forcast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,45 @@
-var forecast = document.querySelector('.forecast');
-var searchButton = document.getElementById('searchbtn');
+var forecast = document.querySelector(".forecast");
+var searchButton = document.getElementById("searchbtn");
 var input = document.querySelector("#searchbox");
 var Cityname = document.querySelector("#cityname");
 var Temp = document.querySelector("#temp");
 var Wind = document.querySelector("#wind");
 var Humidity = document.querySelector("#humidity");
-var Uvindex= document.querySelector("#uv_index");
+var Uvindex = document.querySelector("#uv_index");
 
-searchButton.addEventListener('click', function () {
+searchButton.addEventListener("click", function () {
   var city = input.value;
 
-  var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=3301ee6fc116327db709dae75404298b&units=imperial`;
+  var weatherurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3301ee6fc116327db709dae75404298b&units=imperial`;
 
-  fetch(requestUrl)
+  fetch(weatherurl)
     .then(function (response) {
       // console.log(response);
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
-      Cityname.append(data.city.name);
-      Temp.append(Math.round(data.list[0].main.temp));
-      Wind.append(data.list[0].wind.speed);
-      Humidity.append(data.list[0].main.humidity);
-      
-      }
-    );
-});
+      // console.log(data);
+      Cityname.append(data.name);
+      Temp.append(Math.round(data.main.temp));
+      Wind.append(data.wind.speed);
+      Humidity.append(data.main.humidity);
+
+      let lat = data.coord.lat;
+      let lon = data.coord.lon;
+
+      var onecallurl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=3301ee6fc116327db709dae75404298b&units=imperial`
+
+      fetch(onecallurl)
+      .then(function (response){
+        return response.json();
+      })
+      .then(function (data){
+        
+        console.log(data);
+        Uvindex.append(data.current.uvi);
+      })
+    });
+  });
 
 //** Other fetch syntax */
 // function getApi() {
@@ -46,3 +59,15 @@ searchButton.addEventListener('click', function () {
 //       }
 //     });
 // }
+
+//**** */
+// .then(function (data) {
+//   console.log(data);
+//   Cityname.append(data.name);
+//   Temp.append(Math.round(data.main.temp));
+//   Wind.append(data.wind.speed);
+//   Humidity.append(data.main.humidity);
+
+//   }
+// );
+// });
